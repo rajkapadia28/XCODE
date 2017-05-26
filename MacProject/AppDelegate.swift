a07ccb74e6b6204a56698002db2f25b8ac25b1f0
rajkapadia28 <raj.kapadia28@gmail.com>
2017-05-26 13:07:27 -0400

    Initial Commit

---
+++ MacProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacProject
+//
+//  Created by Raj Kapadia on 5/26/17.
+//  Copyright © 2017 Raj Kapadia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacProject
+//
+//  Created by Raj Kapadia on 5/26/17.
+//  Copyright © 2017 Raj Kapadia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
